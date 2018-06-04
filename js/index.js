2b78fa61094fbf71a3b36ab13216ba03f84af56c
Paolo <paolo@spry-group.com>
2018-06-04 11:25:08 -0400

    Bootstrap initial project

---
+++ js/index.js
@@ -0,0 +1,5 @@
+d3.json('https://sheets.googleapis.com/v4/spreadsheets/1FKNZgxGHjQt9tQ-qmrst1TwK67FgAh7blCL1o-te_3Y/values/A:V?key=AIzaSyA-F8PTqmCvmlWUPmKo8mVMS2siV7kIpZw', init);
+
+function init(error, data) {
+
+}

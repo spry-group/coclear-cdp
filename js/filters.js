4e9fbc07d71cebe5ad93a6b3d9470dea596660c0
Klemensas <hello@klemensas.net>
2019-01-24 21:19:34 +0200

    feat: simplify filters

--- js/filters.js
+++ js/filters.js
@@ -4,32 +4,24 @@ var filters = {
   year: 2013,
   company: 'all'
 }
-function createFilters(data) {
-  const sortSelect = d3.select('#select-sort');
-  const sectorSelect = d3.select('#select-sector');
-  const yearSelect = d3.select('#select-year');
-  const companySelect = d3.select('#select-company');
-  let firstYear = Math.min(...getUniqueValues(data, 'year'));
+var selects = {};
 
-  populateSelect(sectorSelect, getUniqueValues(data, 'sector'));
-  populateSelect(yearSelect, getUniqueValues(data, 'year'));
-  populateSelect(companySelect, getUniqueCompaniesByYear(firstYear));
-  bindSelect(sortSelect, 'sort');
-  bindSelect(sectorSelect, 'sector');
-  bindSelect(yearSelect, 'year');
-  bindSelect(companySelect, 'company');
+function createFilters(data) {
+  selects.sort = d3.select('#select-sort');
+  selects.sector = d3.select('#select-sector');
+  selects.year = d3.select('#select-year');
+  selects.company = d3.select('#select-company');
+  const firstYear = data.reduce((result, item) => Math.min(result, item.year), Infinity);
+
+  bindSelect(selects.sort, 'sort');
+  bindSelect(selects.sector, 'sector');
+  bindSelect(selects.year, 'year');
+  bindSelect(selects.company, 'company');
+  updateData();
   setSelectDefault('year', firstYear);
   setSelectDefault('sort', 'sector');
-  updateData();
 }
 
-// Takes an array of objects and a key to map to.
-// Returns an array of unique values
-function getUniqueValues(data, key) {
- return data.map(d => d[key]).filter((ele, pos, arr) => {
-   return arr.indexOf(ele) === pos;
- }).sort();
-}
 
 function populateSelect(select, data) {
   var options = select.selectAll('option')
@@ -47,86 +39,42 @@ function populateSelect(select, data) {
 
 function bindSelect(ele, key) {
   ele.on('change', function() {
+    console.log('change')
     let val = ele.node().value;
     filters[key] = key !== 'year' || val === 'all' ? val : parseInt(val);
-    avoidConflicts(ele, key);
-    filterRemainingOptions(key, val);
     updateData();
-    // updateQueryParam(key, val);
   });
 }
 
 function updateData() {
   let newData = [...data];
-  ['sector', 'year', 'company'].forEach(key => {
-    if (filters[key] !== 'all') {
-      newData = newData.filter(d => d[key] === filters[key]);
-    }
-  });
-  newData.sort(filters.sort === 'company' ? sortCompany : filters.sort === 'intensity' ? sortIntensity : sortSector);
-  updateChart(newData);
+
+  let filteredData = newData.filter((data) =>
+    (filters.year === 'all' || data.year === filters.year) &&
+    (filters.company === 'all' || data.company === filters.company) &&
+    (filters.sector === 'all' || data.sector === filters.sector)
+  );
+  const yearOptions = new Set(filteredData.map((d) => d.year));
+  const companyOptions = new Set(filteredData.map((d) => d.company));
+  const sectorOptions = new Set(filteredData.map((d) => d.sector));
+  populateSelect(selects.year, Array.from(yearOptions));
+  populateSelect(selects.company, Array.from(companyOptions));
+  populateSelect(selects.sector, Array.from(sectorOptions));
+
+  console.log('wat', newData);
+  filteredData.sort(filters.sort === 'company' ?
+    sortCompany :
+    filters.sort === 'intensity' ?
+      sortIntensity :
+      sortSector
+  );
+  updateChart(filteredData);
 }
 
 function setSelectDefault(key, value) {
   document.getElementById('select-' + key).value = value;
   filters[key] = value;
-}
-
-function avoidConflicts(ele, key) {
-  // // Ignore if we're choosing a single company after filtering down to a sector
-  if (key === 'company' && filters.sector !== 'all' && ele.node().value !== 'all') {
-    return;
-  }
-
-  // If we set sector then reset company, and visa versa
-  if (key === 'sector' || key === 'company') {
-    let conflict = key === 'sector' ? 'company' : 'sector';
-    filters[conflict] = 'all';
-    document.getElementById('select-' + conflict).value = 'all';
-    // updateQueryParam(conflict, 'all');
-  }
-
-  // Restrict company list to companies with data for that year
-  if (key === 'year') {
-    populateCompaniesByYear(ele, key);
-  }
-}
-
-function getUniqueCompaniesByYear(year) {
-  return getUniqueValues(
-    data.filter(d => d.year === year),
-    'company'
-  )
-}
-
-function populateCompaniesByYear(ele, key) {
-  let selectedVal = ele.node().value;
-  const companySelect = d3.select('#select-company');
-
-  if (selectedVal === 'all') {
-    return populateSelect(companySelect, getUniqueValues(data, 'company'));
-  }
-
-  // Show only companies who we have data for that year.
-  selectedVal = parseInt(selectedVal);
-  let companiesInYear = getUniqueCompaniesByYear(selectedVal);
-  populateSelect(companySelect, companiesInYear);
-
-  // If the selected company doesn't have data for this year then reset the company selector
-  if (companiesInYear.indexOf(filters.company) === -1) {
-    setSelectDefault('company', 'all');
-  }
-
-}
-
-function filterRemainingOptions(key, val) {
-  if (key === 'sector' && val !== 'all') {
-    // Only show companies in that sector in the companies select
-    populateSelect(d3.select('#select-company'), getUniqueValues(data.filter(d => d.sector === val), 'company'));
-  } else if ((key === 'company' || key === 'sector') && val === 'all')  {
-    // Add back in all companies when selecting all companies or all sectors
-    populateSelect(d3.select('#select-company'), getUniqueValues(data, 'company'));
-  }
+  console.log('yes', value)
 }
 
 function loadFilters() {

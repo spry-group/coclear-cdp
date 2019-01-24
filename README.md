35606d7502e5aa75e113cedddafc8cc3b5f806bd
Darrel O'Pry <darrel.opry@spry-group.com>
2019-01-24 13:56:00 -0500

    feat: cache json locally to remove dependency on google sheets on release.

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 An interactive radial stacked bar chart depicting cdp product data
 
 # Development
+
+```bash
+# update cached cached data
+npm run update-date
+
+# each of these will need to be ran in a separate console.
+# TODO: make this easier.
 npm run serve
 npm run livereload
 npm run sass
+```
 
 # Deployment
+
 We're using static encryption before launch: https://github.com/robinmoisson/staticrypt
+```bash
+# need to install global, not listed in project dependencies.
 staticrypt index.html passwordHere
+```
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Modified from https://bl.ocks.org/mbostock/6fead6d1378d6df5ae77bb6a719afcb2
+// TODO: eliminate globals.
 var data = [];
 var copy; // Written copy from the spreadsheet
 var companies;
@@ -6,95 +6,23 @@ var sectors;
 var years;
 
 document.addEventListener('DOMContentLoaded', function(e) {
-  d3.json('https://sheets.googleapis.com/v4/spreadsheets/1FKNZgxGHjQt9tQ-qmrst1TwK67FgAh7blCL1o-te_3Y/values/A:AZ?key=AIzaSyA-F8PTqmCvmlWUPmKo8mVMS2siV7kIpZw', init);
+  d3.json('content.json', init);
 });
 
-function init(error, sourceData) {
+function init(error, content) {
   if (error) {
     return alert('Something went wrong, please try again later.');
   }
-  cleanAndParseData(sourceData);
-  updateCopy();
-  createChart(data);
-  createFilters(data);
+  data = content.data;
+  copy = content.copy;
+  updateCopy(content.copy);
+  createChart(content.data);
+  createFilters(content.data);
   loadFilters();
 }
 
-function cleanAndParseData(sourceData) {
-  sourceData = sourceData.values;
-  sourceData.shift(); // Discard copyright notice
-  let cols = sourceData.shift();
-  let colIndexes = {
-    company: cols.indexOf('Organisation'),
-    country: cols.indexOf('Country'),
-    name: cols.indexOf('Product name'),
-    id: cols.indexOf('CoClear-Product ID'),
-    desc: cols.indexOf('Product detail'),
-    footprint: cols.indexOf('Product CO2e footprint (kg CO2e)'),
-    upstream: cols.indexOf('CoClear-Footprint %Upstream'),
-    operation: cols.indexOf('CoClear-Footprint %Operation'),
-    downstream: cols.indexOf('CoClear-Footprint %Downstream'),
-    transportation: cols.indexOf('CoClear-Footprint %Transportion'),
-    endOfLife: cols.indexOf('CoClear-Footprint %EndOfLife'),
-    sector: cols.indexOf('CoClear-Sector Mapping'),
-    year: cols.indexOf('Year of reporting'),
-    carbonInt: cols.indexOf('CoClear-Product Carbon Intensity'),
-    weight: cols.indexOf('Product FU Weight (kg)'),
-    weightSource: cols.indexOf('CoClear Product Weight Source'),
-    dataCorrections: cols.indexOf('CoClear DataCorrections Comment'),
-    protocol: cols.indexOf('CoClear Protocol Mapping'),
-    footprintChangePer: cols.indexOf('Footprint %Change'),
-    footprintChangeCategory: cols.indexOf('CoClear-Change Category'),
-    footprintChangeReason: cols.indexOf('Reason for change'),
-  }
 
-  copy = {
-    title: sourceData[0][cols.indexOf('Title Text')],
-    header: sourceData[0][cols.indexOf('Header Text')],
-    footer1: sourceData[0][cols.indexOf('Footer Text1')],
-    footer2: sourceData[0][cols.indexOf('Footer Text2')],
-    footerImg: sourceData[0][cols.indexOf('Footer Img URL')],
-  }
-
-  data.push(...sourceData.map((row) => mapRow(colIndexes, row)));
-  data.sort(sortCompany);
-}
-
-function mapRow(colIndexes, row) {
-  let obj = {
-    company: row[colIndexes.company],
-    country: row[colIndexes.country],
-    name: row[colIndexes.name],
-    coclearId: row[colIndexes.id],
-    id: row[colIndexes.id] + row[colIndexes.year],
-    desc: row[colIndexes.desc],
-    sector: row[colIndexes.sector],
-    year: parseInt(row[colIndexes.year]),
-    footprint: parseFloat(row[colIndexes.footprint]),
-    footprintLabel: row[colIndexes.footprint],
-    upstreamPer: row[colIndexes.upstream],
-    manufacturingPer: row[colIndexes.operation],
-    downstreamPer: row[colIndexes.downstream],
-    carbonInt: parseFloat(row[colIndexes.carbonInt]),
-    carbonIntLabel: row[colIndexes.carbonInt],
-    weight: row[colIndexes.weight],
-    weightSource: row[colIndexes.weightSource],
-    dataCorrections: row[colIndexes.dataCorrections],
-    protocol: row[colIndexes.protocol],
-    footprintChangePer: parseFloat(row[colIndexes.footprintChangePer]),
-    footprintChangePerLabel: row[colIndexes.footprintChangePer].replace('-', ''),
-    footprintChangeCategory: row[colIndexes.footprintChangeCategory],
-    footprintChangeReason: row[colIndexes.footprintChangeReason],
-  };
-  obj['upstream']                 = ((parseFloat(row[colIndexes.upstream]) || 0) / 100) * obj.carbonInt;
-  obj['manufacturing']            = ((parseFloat(row[colIndexes.operation]) || 0) / 100) * obj.carbonInt;
-  obj['downstream']               = ((parseFloat(row[colIndexes.downstream]) || 0) / 100) * obj.carbonInt;
-  obj['stage data not available'] = (obj['upstream'] + obj['manufacturing'] + obj['downstream']) === 0 ?
-                                      obj.carbonInt : 0;
-  return obj;
-}
-
-function updateCopy() {
+function updateCopy(copy) {
   document.getElementById('title-text').innerHTML = copy.title;
   document.getElementById('header-text').innerHTML = copy.header;
   document.getElementById('footer-text1').innerHTML = copy.footer1;
